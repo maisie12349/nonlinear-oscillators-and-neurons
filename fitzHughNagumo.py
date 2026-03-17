0bd99540f89092c31443a47e311773f9b579d1ab
Josiah Burden <josiahburden19@gmail.com>
2026-03-17 01:34:12 +0000

    Separate Graphs

--- fitzHughNagumo.py
+++ fitzHughNagumo.py
@@ -8,32 +8,47 @@ from helpers import *
 
 def fitzHughNagumoModel(
     x0: float, y0: float, dt: float, endTime: float, params: ModelParams
-) -> tuple[float, float]:
+) -> tuple[np.array[float, float], np.array[float]]:
     positionValues = [np.array([x0, y0])]
     numSteps = int(endTime / dt)
+    timeValues = [0]
 
-    for i in range(numSteps):
+    for _ in range(numSteps):
         # Gets the dx/dt and dy/dt functions with the parameters so we can time step the model
         # The euler method requires a dx_dt and dy_dt function with two parameters (x, y)
         # So by using dx_dtWithParams and dy_dtWithParams, we can pass the parameters (I, mu, a, b) to the functions
         dx_dt = dx_dtWithParams(params.I)
         dy_dt = dy_dtWithParams(params.mu, params.a, params.b)
+        prevPosition = positionValues[-1]
+        prevTime = timeValues[-1]
         # Run the euler method with the current position and the parameters
-        x, y = euler_step_2d(
-            # positionValues[-1] indicates the last position, 
-            # 0 indicates the x value 
-            # 1 indicates the y value
-            positionValues[-1][0], positionValues[-1][1], dt, dx_dt, dy_dt
+        eulerX, eulerY = euler_step_2d(
+            prevPosition[0], prevPosition[1], dt, dx_dt, dy_dt
         )
-        # Append the new position to the list of position values
-        positionValues.append(np.array([x, y]))
+        time = prevTime + dt
+        positionValues.append(np.array([eulerX, eulerY]))
+        timeValues.append(time)
 
     # Return the position values as a numpy array
-    return np.array(positionValues)
+    return np.array(positionValues), np.array(timeValues)
+
+
+def plotFormatting(
+    axs: plt.Axes, xLimits: tuple[float, float], yLimits: tuple[float, float]
+):
+    axs.set_xlim(xLimits)
+    axs.set_ylim(yLimits)
+    axs.set_xlabel("x(t)")
+    axs.set_ylabel("y(t)")
+    axs.grid(True, color="gray", alpha=0.2)
+    axs.set_aspect("equal")
+    axs.set_xticks(np.arange(xLimits[0], xLimits[1] + 0.1, 1))
+    axs.set_yticks(np.arange(yLimits[0], yLimits[1] + 0.1, 1))
+
 
 # Main function
-# This code here checks if the file is being run directly and not imported as a module
 if __name__ == "__main__":
+    setFont()
     # Define the parameters for the FitzHugh-Nagumo model
     IValues = np.linspace(0, 2, 3)
     mu = 10
@@ -43,35 +58,71 @@ if __name__ == "__main__":
     y0 = 0
     dt = 0.1
     endTime = 100
-    xAxisValues = np.linspace(-2, 2, 100)
+    xLimits = (-3, 4)
+    yLimits = (-2, 3.5)
+    xAxisValues = np.linspace(xLimits[0], xLimits[1], 100)
 
-    fig, axs = plt.subplots()
-    # Run the FitzHugh-Nagumo model for each value of I
-    for I in IValues:
-        # Create the parameters for the FitzHugh-Nagumo model
-        params = ModelParams(I, mu, a, b)
-        # Run the FitzHugh-Nagumo model for the current value of I
-        positionValues = fitzHughNagumoModel(
-            x0, y0, dt, endTime, params
-        )
-        # Plot the position values on a graph
-        xValues = positionValues[:, 0]
-        yValues = positionValues[:, 1]
-        axs.plot(xValues, yValues)
-        xNullcline = xNullclineWithParams(I)
-        xNullclineValues = xNullcline(xAxisValues)
-        axs.plot(xAxisValues, xNullclineValues, linestyle = "--", color = "red", label = f"x Nullcline I = {I}")
+    colors = {
+        0: ["#62A1A4", "#568C8F"],
+        1: ["#F2AD00", "#FFA244"],
+        2: ["#AD84FF", "#843EB9"],
+    }
 
-    yNullcline = yNullclineWithParams(a, b)
-    yNullclineValues = yNullcline(xAxisValues)
-    axs.plot(xAxisValues, yNullclineValues, linestyle = "--", color = "blue", label = f"y Nullcline")
-    axs.legend()
-    axs.set_title("FitzHugh-Nagumo Model Phase Portrait")
-    axs.set_xlabel("X")
-    axs.set_ylabel("Y")
-    # Save the figure
     # Check if the directory exists, if not create it
     if not os.path.exists("FitzHughNagumo"):
         os.makedirs("FitzHughNagumo")
 
-    fig.savefig("FitzHughNagumo/fitzHughNagumo.png", dpi=300)
+    # Run the FitzHugh-Nagumo model for each value of I
+    for index, I in enumerate(IValues):
+        fig, axs = plt.subplots()
+
+        # Create the parameters for the FitzHugh-Nagumo model
+        params = ModelParams(I, mu, a, b)
+        # Run the FitzHugh-Nagumo model for the current value of I
+        eulerPositionValues, timeValues = fitzHughNagumoModel(
+            x0, y0, dt, endTime, params
+        )
+        # Plot the position values on a graph
+        eulerXValues = eulerPositionValues[:, 0]
+        eulerYValues = eulerPositionValues[:, 1]
+
+        axs.plot(
+            eulerXValues,
+            eulerYValues,
+            color=colors[index][1],
+            label=f"Trajectory",
+            zorder=10,
+        )
+
+        xNullcline = xNullclineWithParams(I)
+        xNullclineValues = xNullcline(xAxisValues)
+        axs.plot(
+            xAxisValues,
+            xNullclineValues,
+            linestyle="--",
+            color=colors[index][0],
+            label=f"dx/dt = 0",
+            alpha=0.7,
+            zorder=5,
+        )
+
+        yNullcline = yNullclineWithParams(a, b)
+        yNullclineValues = yNullcline(xAxisValues)
+        axs.plot(
+            xAxisValues,
+            yNullclineValues,
+            linestyle="--",
+            color="#717171",
+            label=f"dy/dt = 0",
+            alpha=0.7,
+            zorder=5,
+        )
+
+        axs.legend(loc="upper right")
+
+        plotFormatting(axs, xLimits, yLimits)
+        axs.set_title(f"I = {I} FitzHugh-Nagumo Model Phase Portrait")
+
+        fig.savefig(
+            f"FitzHughNagumo/fitzHughNagumo_I={I}.png", dpi=300, bbox_inches="tight"
+        )
